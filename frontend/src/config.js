52af3916c8fd1cad226068afd46d049cc65acf85
hanisah12 <siddhihanisah2007@gmail.com>
2026-05-16 21:33:04 +0530

    fix : backend url changed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
-const API_BASE = "https://click2pass-fullstack.onrender.com"
+const API_BASE = "https://click2pass-fullstack.onrender.com/api"
+// const API_BASE = "http://127.0.0.1:8000/api"
 
 
 export default API_BASE;
